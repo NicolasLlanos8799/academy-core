e5f4a175eff63bcf391c67368aeea65703a3a3f4
Nicolas Llanos <nicollanos8799@gmail.com>
2025-06-17 15:07:52 +0200

    validar sesion separado

--- admin.php
+++ admin.php
@@ -1,28 +1,6 @@
 <?php
 $config = include __DIR__ . '/php/school_config.php';
-session_start(); // 🔑 Siempre primero
-// Control de inactividad
-$tiempoInactividad = 129600; // 36 horas
-
-if (isset($_SESSION['ULTIMA_ACTIVIDAD']) && (time() - $_SESSION['ULTIMA_ACTIVIDAD']) > $tiempoInactividad) {
-    session_unset();     // Limpia variables de sesión
-    session_destroy();   // Elimina la sesión
-    header("Location: login.php?expirada=1");
-    exit;
-}
-
-$_SESSION['ULTIMA_ACTIVIDAD'] = time(); // Renueva el tiempo de actividad
-
-// 🔒 Bloquear caché del navegador
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-header("Expires: 0");
-
-// 🔐 Validar sesión y rol
-if (!isset($_SESSION['user_id']) || $_SESSION['rol'] !== 'admin') {
-    header("Location: login.php");
-    exit;
-}
+require_once __DIR__ . '/php/validar_sesion_admin.php';
 ?>
 
 <!DOCTYPE html>
